d17e787defe3ffa848a70ebe9175423604cc16c6
Julia <80031598+JuliaStepanenko@users.noreply.github.com>
2021-06-23 00:28:33 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// Домашка #17 — Events
+
+// На странице находится инпут и кнопка. Пользователь может ввести что-то в инпут и нажать на кнопку,
+// после этого в списке ниже должна отобразится строка с тем, что было введено в инпуте.
+// После этого инпут очищается.
+// Если пользователь добавляет еще текст, он добавляется НАД предыдущим.
+// Список должен быть сделан через ul > li.
+
+let list = document.querySelector('.list');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function (event) {
+
+    if (document.querySelector('input').value != false){
+
+        let li = document.createElement('li');
+        li.innerHTML = document.querySelector('input').value;
+        list.prepend(li);
+        document.querySelector('input').value = '';
+
+    } else alert('Сначала введите текст')
+
+});
